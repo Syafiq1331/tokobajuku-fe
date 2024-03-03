1deb17b032915933ec75093a231306b1c1a56be9
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-03 13:03:10 +0700

    update Layout.tsx and Sidebar.tsx

--- src/components/admin/layout/Layout.tsx
+++ src/components/admin/layout/Layout.tsx
@@ -3,17 +3,18 @@ import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 interface LayoutProps {
-  children: ReactNode;
+  children: ReactNode,
+  className?: string
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, className }: LayoutProps) => {
   return (
     <div className='grid grid-cols-12 h-full'>
-      <Sidebar className='lg:col-span-2 lg:block hidden'/>
+      <Sidebar className='lg:col-span-2 lg:block hidden' />
       <div className='lg:col-span-10 col-span-12'>
         <Navbar />
-        <div className='px-10 py-4 -mt-14'>
-        {children}
+        <div className={`px-10 py-4 ${className}`}>
+          {children}
         </div>
       </div>
     </div>
--- src/components/admin/layout/Sidebar.tsx
+++ src/components/admin/layout/Sidebar.tsx
@@ -1,45 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Sidebar } from 'flowbite-react';
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+import Link from 'next/link';
 
 interface SidebarProps {
     className?: string
 }
 
-const sidebar = ({className}: SidebarProps) => {
-  return (
-<div className={`h-screen bg-[#F9FAFB] flex ${className}`}>
-    <Sidebar aria-label="Default sidebar example" className='h-full'>
-        <Sidebar.Items className='h-full'>
-            <Sidebar.Logo href="#" img="/favicon.ico" imgAlt="Flowbite logo">
-                Flowbite
-            </Sidebar.Logo>
-            <Sidebar.ItemGroup>
-                <Sidebar.Item href="#" icon={HiChartPie}>
-                    Dashboard
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiViewBoards}>
-                    Kanban
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiInbox}>
-                    Inbox
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiUser}>
-                    Users
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiShoppingBag}>
-                    Products
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiArrowSmRight}>
-                    Sign In
-                </Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiTable}>
-                    Sign Up
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
-    </Sidebar>
-</div>
+const sidebar = ({ className }: SidebarProps) => {
+    const [dynamicClass, setDynamicClass] = useState('');
+
+    useEffect(() => {
+        const handleResize = () => {
+            const pageHeight = document.documentElement.scrollHeight;
+            const windowHeight = window.innerHeight;
+
+            if (pageHeight > windowHeight) {
+                setDynamicClass('h-full');
+            } else {
+                setDynamicClass('h-screen');
+            }
+        };
+
+        if (typeof window !== 'undefined') {
+            handleResize();
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    return (
+        <div className={`bg-[#F9FAFB] flex ${dynamicClass} ${className}`}>
+
+            <Sidebar aria-label="Default sidebar example" className='h-full'>
+                <Sidebar.Items className='h-full'>
+                    <Sidebar.Logo href="#" img="/favicon.ico" imgAlt="Flowbite logo">
+                        Tokobajuku
+                    </Sidebar.Logo>
+                    <Sidebar.ItemGroup>
+                        <Sidebar.Item icon={HiChartPie}>
+                            <Link href="/admin/dashboard/app">
+                                Dashboard
+                            </Link>
+                        </Sidebar.Item>
+                        <Sidebar.Item icon={HiViewBoards}>
+                            <Link href="/admin/product/app">
+                                Product
+                                <span>
+                                    <a href="#"></a>
+                                </span>
+                            </Link>
+                        </Sidebar.Item>
+                        <Sidebar.Item icon={HiInbox}>
+                            <Link href="/admin/category/app">
+                                Category
+                                <span>
+                                    <a href="#"></a>
+                                </span>
+                            </Link>
+                        </Sidebar.Item>
+                        <Sidebar.Item icon={HiUser}>
+                            <Link href="/admin/etalase/app">
+                                Etalase
+                                <span>
+                                    <a href="#"></a>
+                                </span>
+                            </Link>
+                        </Sidebar.Item>
+                        <Sidebar.Item icon={HiShoppingBag}>
+                            <Link href="/admin/monitoring/app">
+                                Monitoring Admin
+                                <span>
+                                    <a href="#"></a>
+                                </span>
+                            </Link>
+                        </Sidebar.Item>
+                        <Sidebar.Item icon={HiArrowSmRight}>
+                            <Link href="/admin/keuangan/app">
+                                Perhitungan Keuangan
+                                <span>
+                                    <a href="#"></a>
+                                </span>
+                            </Link>
+                        </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                </Sidebar.Items>
+            </Sidebar>
+        </div>
 
     )
 }
