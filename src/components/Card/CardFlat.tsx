e060ebae9d5cb55221a8c9216d1fa294a84b2e25
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-03 13:03:05 +0700

    update CardFlat.tsx and CardWithTitle.tsx

--- src/components/Card/CardFlat.tsx
+++ src/components/Card/CardFlat.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 interface LayoutProps {
-    children: React.ReactNode,
-    title : React.ReactNode,
-    className?: string
+    children?: React.ReactNode,
+    title?: React.ReactNode,
+    className?: string,
+    classNameBody?: string
 }
 
-const CardFlat = ({children, title, className} : LayoutProps ) => {
+const CardFlat = ({ children, title, className, classNameBody }: LayoutProps) => {
     return (
         <div className={`border bg-white p-4 ${className}`}>
-            <h4>{title}</h4>
+            <h4 className={`${classNameBody}`}>{title}</h4>
             {children}
         </div>
     )
--- src/components/Card/CardWithTitle.tsx
+++ src/components/Card/CardWithTitle.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const CardWithTitle = () => {
-  return (
-    <div>CardWithTitle</div>
-  )
+interface LayoutProps {
+    children: React.ReactNode,
+    title : React.ReactNode,
+    className?: string
+}
+
+const CardWithTitle = ({children, title, className} : LayoutProps ) => {
+    return (
+        <div className={`border bg-white ${className}`}>
+            <h4 className='p-4'>{title}</h4>
+            <hr />
+            <div className='p-4'>
+            {children}
+            </div>
+        </div>
+    )
 }
 
 export default CardWithTitle
