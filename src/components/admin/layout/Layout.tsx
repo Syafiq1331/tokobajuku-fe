220a6cbe47b7415b239a4bda86dca8b64e466f86
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-04 08:43:43 +0700

    update Layout.tsx and Navbar.tsx

--- src/components/admin/layout/Layout.tsx
+++ src/components/admin/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
@@ -8,17 +8,33 @@ interface LayoutProps {
 }
 
 const Layout = ({ children, className }: LayoutProps) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleNavbarClick = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className='grid grid-cols-12 h-full'>
-      <Sidebar className='lg:col-span-2 lg:block hidden' />
-      <div className='lg:col-span-10 col-span-12'>
-        <Navbar />
+      <div className={`lg:col-span-2 fixed inset-y-0 left-0 z-50 bg-gray-200 overflow-hidden transition-transform duration-300 ease-in-out transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        <Sidebar />
+      </div>
+      <div className={`lg:col-span-12 col-span-12 transition-all duration-300 ease-in-out ${isSidebarOpen ? 'lg:ml-64' : 'lg:ml-0'}`}>
+        <Navbar onClick={handleNavbarClick} />
         <div className={`px-10 py-4 ${className}`}>
           {children}
         </div>
       </div>
+      {/* Overlay to close sidebar when clicked outside */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden"
+          onClick={handleNavbarClick}
+        ></div>
+      )}
     </div>
   );
 };
 
 export default Layout;
+
--- src/components/admin/layout/Navbar.tsx
+++ src/components/admin/layout/Navbar.tsx
@@ -2,21 +2,25 @@ import Image from 'next/image';
 import React from 'react';
 import { FaBars } from "react-icons/fa6";
 
-const Navbar = () => {
+interface NavbarProps {
+  onClick: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ onClick }) => {
   return (
-    <nav className=' bg-[#4ADE80] w-full px-10 py-20 pt-10 flex justify-between'>
-      <FaBars className='text-2xl text-white'/>
-      <div className='flex gap-x-2'>
-      <Image
-      src="/admin/avatar.png"
-      width={24}
-      height={25}
-      alt="Picture of the author"
-    />
-        <p className='text-white'>Hi, ujang udin</p>
+    <nav className='bg-[#4ADE80] w-full px-10 py-16 flex justify-between items-center'>
+      <FaBars className='text-2xl text-white cursor-pointer' onClick={onClick} />
+      <div className='flex gap-x-2 items-center'>
+        <Image
+          src="/admin/avatar.png"
+          width={24}
+          height={25}
+          alt="Avatar"
+        />
+        <p className='text-white'>Hi, Ujang Udin</p>
       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
