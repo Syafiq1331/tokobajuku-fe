2bd3f9bf3531d95a0cecdda62f67613e93daa223
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-11 17:34:25 +0700

    update Register.tsx and login.tsx

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+import Image from 'next/image';
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="flex lg:flex-nowrap flex-wrap h-screen gap-x-32 items-center">
+      <div className='w-full lg:p-12 p-6 bg-backgroundLogin'>
+        <Image
+          src="/auth/register.svg"
+          alt=""
+          width={100}
+          height={100}
+          layout="responsive"
+          sizes="(max-width: 640px) 200px, 500px"
+        />
+      </div>
+      <div className='w-full lg:mx-0 mx-12 mt-12 lg:mt-0'>
+        <h3 className="text-medium text-2xl">Buat akun baru</h3>
+        <p className='mt-1'>Isi data di bawah dengan benar</p>
 
-export default Register
+        <div className='lg:mr-44 mt-4'>
+          <form className="flex flex-col gap-4">
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="nama" value="Nama" />
+              </div>
+              <TextInput id="nama" type="text" placeholder="Agus Saepulloh" required className='w-full' />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email" value="Email anda" />
+              </div>
+              <TextInput id="email" type="email" placeholder="agus@gmail.com" required />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="password" value="Password anda" />
+              </div>
+              <TextInput id="password" type="password" required />
+            </div>
+            <Button className='bg-primary/80  enabled:hover:bg-primary focus:ring-4 focus:ring-primary dark:bg-primary dark:enabled:hover:bg-primary dark:focus:ring-primary dark:border-primary' type="submit">Submit</Button>
+          </form>
+          <p className='text-center mt-14 text-sm'>
+            Sudah punya akun ?
+            <a href="login" className='ml-1'>
+              <span className='font-[600]'>Login</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,33 +4,44 @@ import Image from 'next/image';
 
 const Login = () => {
     return (
-        <div className="flex h-screen gap-x-32 items-center">
-            <div className='w-full'>
-                <Image src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="" className='py-24' />
+        <div className="flex lg:flex-nowrap flex-wrap h-screen gap-x-32 items-center">
+            <div className='w-full lg:p-12 p-6 bg-backgroundLogin'>
+                <Image
+                    src="/auth/login.svg"
+                    alt=""
+                    width={100}
+                    height={100}
+                    layout="responsive"
+                    sizes="(max-width: 640px) 200px, 500px"
+                />
             </div>
-            <div className='w-full'>
+            <div className='w-full lg:mx-0 mx-12'>
                 <h3 className="text-medium text-2xl">Login ke <strong>Tokobajuku</strong></h3>
-                <p>Isi data di bawah dengan benar</p>
+                <p className='mt-1'>Isi data di bawah dengan benar</p>
 
-                <form className="flex w-full flex-col gap-4">
-                    <div>
-                        <div className="mb-2 block">
-                            <Label htmlFor="email1" value="Your email" />
+                <div className='lg:mr-44  mt-4'>
+                    <form className="flex flex-col gap-4">
+                        <div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="email1" value="Nama email" />
+                            </div>
+                            <TextInput id="email1" type="email" placeholder="agus@gmail.com" required className='w-full' />
                         </div>
-                        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required className='w-full' />
-                    </div>
-                    <div>
-                        <div className="mb-2 block">
-                            <Label htmlFor="password1" value="Your password" />
+                        <div>
+                            <div className="mb-2 block">
+                                <Label htmlFor="password1" value="Password" />
+                            </div>
+                            <TextInput id="password1" type="password" required />
                         </div>
-                        <TextInput id="password1" type="password" required />
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Checkbox id="remember" />
-                        <Label htmlFor="remember">Remember me</Label>
-                    </div>
-                    <Button type="submit">Submit</Button>
-                </form>
+                        <Button className='bg-primary/80  enabled:hover:bg-primary focus:ring-4 focus:ring-primary dark:bg-primary dark:enabled:hover:bg-primary dark:focus:ring-primary dark:border-primary' type="submit">Submit</Button>
+                    </form>
+                    <p className='text-center mt-14 text-sm'>
+                        Belum punya akun ?
+                        <a href="register" className='ml-1'>
+                            <span className='font-[600]'>Daftar</span>
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     );
