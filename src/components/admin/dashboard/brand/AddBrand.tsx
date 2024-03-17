1e5b9a5070345ac2c8f8f4288e826818bbd5d8d9
Syafiq1331 <bussinesyafiq@gmail.com>
2024-03-17 21:54:03 +0700

    feat: create AddBrand.tsx and ListBrand.tsx

---
+++ src/components/admin/dashboard/brand/AddBrand.tsx
@@ -0,0 +1,71 @@
+import CardWithTitle from '@/components/Card/CardWithTitle'
+import React, { useState } from 'react'
+import { TextInput, Label, Select } from 'flowbite-react';
+import { useRouter } from 'next/router';
+
+const AddBrand = () => {
+    const [namaKategori, setNamaKategori] = useState('');
+    const [statusKategori, setStatusKategori] = useState('published'); // Default status
+
+    const router = useRouter();
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://tokobajuku.syafiqrzf.my.id/api/brand', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: namaKategori,
+                    status: statusKategori,
+                }),
+            });
+            if (response.ok) {
+                // Redirect ke halaman lain setelah berhasil menambahkan
+                router.push('/admin/brand/app');
+            } else {
+                // Handle kesalahan jika ada
+                console.error('Gagal menambahkan kategori');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <CardWithTitle className='my-5 lg:col-span-8 col-span-12' title="Tambah Brand">
+            <form onSubmit={handleSubmit}>
+                <div className="grid grid-cols-12 gap-6">
+                    <div className='col-span-6'>
+                        <div className="mb-2 block">
+                            <Label htmlFor="text" value="Nama Brand" />
+                        </div>
+                        <TextInput id="text" type="text" placeholder="Distro Ubuntu" value={namaKategori} onChange={(e) => setNamaKategori(e.target.value)} required />
+                    </div>
+                    <div className="max-w-md col-span-6">
+                        <div className="mb-2 block">
+                            <Label htmlFor="countries" value="Status Brand" />
+                        </div>
+                        <Select id="countries" value={statusKategori} onChange={(e) => setStatusKategori(e.target.value)} required>
+                            <option value="published">Published</option>
+                            <option value="unpublished">Unpublished</option>
+                        </Select>
+                    </div>
+                </div>
+
+                <hr className='mt-12 mb-4' />
+
+                <div className="flex justify-end">
+                    <button type="submit" className='bg-primary text-white px-4 py-2 rounded-lg'>
+                        Tambah Brand
+                    </button>
+                </div>
+            </form>
+        </CardWithTitle>
+    )
+}
+
+export default AddBrand;
---
+++ src/components/admin/dashboard/brand/ListBrand.tsx
@@ -0,0 +1,124 @@
+import CardWithTitle from '@/components/Card/CardWithTitle';
+import React, { useState, useEffect } from 'react';
+import { HiPencil, HiTrash, HiOutlineInformationCircle } from 'react-icons/hi';
+import { Table } from 'flowbite-react';
+import Link from 'next/link';
+
+const ListBrand = () => {
+    const [pageNumber, setPageNumber] = useState(0);
+    const [brandData, setBrandData] = useState([]);
+    const usersPerPage = 5;
+    const pagesVisited = pageNumber * usersPerPage;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://tokobajuku.syafiqrzf.my.id/api/brand');
+                const data = await response.json();
+                setBrandData(data.data);
+            } catch (error) {
+                console.error('Error fetching brand data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const pageCount = Math.ceil(brandData.length / usersPerPage);
+
+    const changePage = (selectedPage: any) => {
+        setPageNumber(selectedPage);
+    };
+
+    const handleDelete = async (id: any) => {
+        try {
+            const response = await fetch(`http://tokobajuku.syafiqrzf.my.id/api/brand/${id}`, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                // Refresh data setelah berhasil menghapus
+                const newData = brandData.filter((item: { id: any }) => item.id !== id);
+                setBrandData(newData);
+            } else {
+                console.error('Gagal menghapus brand');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <>
+            <div className='flex justify-end'>
+                <Link href="/admin/brand/add-brand">
+                    <button className='bg-primary text-white px-4 py-2 rounded-lg'>
+                        Tambah Brand
+                    </button>
+                </Link>
+            </div>
+            <CardWithTitle className='my-5 lg:col-span-4 col-span-12' title="List Brand">
+                <div className="overflow-x-auto">
+                    <Table>
+                        <Table.Head>
+                            <th className='text-start pl-6 text-secondary'>No</th>
+                            <th className='text-start pl-6 text-secondary'>Nama</th>
+                            <th className='text-start pl-6 text-secondary'>Status Brand</th>
+                            <th className='pl-6 text-secondary text-center'>Action</th>
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            {brandData
+                                .slice(pagesVisited, pagesVisited + usersPerPage)
+                                .map((item: any, index) => (
+                                    <Table.Row key={item.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                        <Table.Cell>{index + 1}</Table.Cell>
+                                        <Table.Cell>{item.name}</Table.Cell>
+                                        <Table.Cell>
+                                            {item.status === "published" ? (
+                                                <button className="bg-primary text-white py-2 px-4 rounded-lg w-full">
+                                                    Published
+                                                </button>
+                                            ) : (
+                                                <button className="bg-third text-white py-2 px-4 rounded-lg w-full">
+                                                    Unpublished
+                                                </button>
+                                            )}
+                                        </Table.Cell>
+                                        <Table.Cell className='text-center'>
+                                            {/* Tombol Delete */}
+                                            <button onClick={() => handleDelete(item.id)} className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-lg mr-2">
+                                                <HiTrash className="inline-block mr-1" />
+                                                Delete
+                                            </button>
+                                            {/* Tombol Edit */}
+                                            <button className="bg-third hover:bg-blue-700 text-white py-2 px-4 rounded-lg mr-2">
+                                                <HiPencil className="inline-block mr-1" />
+                                                Edit
+                                            </button>
+                                            {/* Tombol Detail */}
+                                            <button className="bg-primary hover:bg-gray-700 text-white py-2 px-4 rounded-lg">
+                                                <HiOutlineInformationCircle className="inline-block mr-1" />
+                                                Detail
+                                            </button>
+                                        </Table.Cell>
+                                    </Table.Row>
+                                ))}
+                        </Table.Body>
+                    </Table>
+                    <div className="flex justify-center mt-4">
+                        {Array.from({ length: pageCount }, (_, index) => (
+                            <button
+                                key={index}
+                                className={`mx-1 px-3 py-1 rounded-lg border ${pageNumber === index ? 'bg-primary text-white' : 'bg-white text-black hover:bg-gray-200'}`}
+                                onClick={() => changePage(index)}
+                            >
+                                {index + 1}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </CardWithTitle>
+        </>
+    );
+};
+
+export default ListBrand;
